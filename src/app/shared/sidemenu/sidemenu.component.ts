e383107909e5e2d28b420a997ce0527eace0ba5d
zotel1 <zotelsigel@gmail.com>
2024-08-05 06:03:16 -0300

    update: terminamos de configurar nuestro dashboard

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
+    selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidemenu.component.html',
   styles: ``
 })
 export class SidemenuComponent {
+
+    public menuItems = routes
+        .map((route) => route.children ?? [])
+        .flat()
+        .filter((route) => route && route.path)
+        .filter((route) => !route.path?.includes(':'));
+
+    constructor() {
+        
+        //const dashboardRoutes = routes
+        //.map( route => route.children ?? [] )
+       // .flat()
+       // .filter( route => route && route.path )
+       // .filter( route => !route.path?.includes(':') )
+       // console.log(dashboardRoutes)
+
+    }
 
 }

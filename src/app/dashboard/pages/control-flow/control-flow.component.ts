40c033e87ff8338fcf0e69beb906ae59555f01ac
zotel1 <zotelsigel@gmail.com>
2024-08-05 10:26:23 -0300

    agregamos un boton para probar el if

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
   styles: ``
 })
 export default class ControlFlowComponent {
+
+    public showContent = signal(false);
+
+    public toggleContent() {
+        this.showContent.update( value => !value)
+    }
 
 }

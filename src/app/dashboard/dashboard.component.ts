fbbd466f9264c3574e8bdae0c9d1c835614b4796
zotel1 <zotelsigel@gmail.com>
2024-08-04 23:31:19 -0300

    update: rutas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './dashboard.component.html',
   styles: ``
 })

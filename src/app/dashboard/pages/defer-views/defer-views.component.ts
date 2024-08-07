feaffd1f817bf12633db557d1f2562cf9fd8b433
zotel1 <zotelsigel@gmail.com>
2024-08-07 11:04:32 -0300

    update: defer- triggers

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from "../../../shared/title/title.component";
 
 @Component({
   standalone: true,
-  imports: [CommonModule, HeavyLoadersSlowComponent],
+  imports: [CommonModule, HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html',
   styles: ``
 })

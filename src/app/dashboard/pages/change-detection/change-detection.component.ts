e4bf2091412e0011b61073865fb3f182293c8cf3
zotel1 <zotelsigel@gmail.com>
2024-08-07 09:23:21 -0300

    update: adaptamos los cambios  angular 18

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+  <app-title title="Change Detection" />
+
+  <pre> {{framewokAsSignal() | json }} </pre>
+  <pre> {{ framewokAsProperty | json }}</pre>
+  `,
 })
 export default class ChangeDetectionComponent {
+
+    public framewokAsSignal = signal({
+        name: 'Angular',
+        releaseDate: 2016,
+    })
+    public framewokAsProperty = {
+        name: 'Angular',
+        releaseDate: 2016,
+    }
+
+    constructor() {
+        setTimeout(() => {
+
+            this.framewokAsProperty.name = 'React';
+
+            console.log('Hecho')
+        }, 3000);
+    }
 
 }

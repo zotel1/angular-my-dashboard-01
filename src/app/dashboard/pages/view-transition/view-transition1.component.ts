42c2ac98ad52ffd6ac1cbdd5f32b3e60ad129cad
zotel1 <zotelsigel@gmail.com>
2024-08-11 10:02:33 -0300

    update: view transition 2

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -7,13 +7,18 @@ import { TitleComponent } from '@shared/title/title.component';
   imports: [CommonModule, TitleComponent],
   template: `
   <app-title title="View Transition 2"/>
-  <section class="flex justify-start">
+  <section class="flex justify-end">
     <img 
     src="assets/atenea.jpeg"
     alt="Perrita-linda"
     width="200"
     height="300"
     />
+    <div
+    class="bg-blue-500 w-56 h-56"
+    >
+    </div>
+  </section>
   `
 })
 export default class ViewTransitionComponent {

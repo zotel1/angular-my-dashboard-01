00341b5fb1e106196e7d5fc4dbb0cda30626431f
zotel1 <zotelsigel@gmail.com>
2024-08-13 20:51:45 -0300

    update: peticiones http

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent, RouterModule],
   templateUrl: './users.component.html',
   styles: ``
 })
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,6 +1,7 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { User, UsersResponse } from '../interfaces/req-response';
 import { HttpClient } from '@angular/common/http';
+import { delay } from 'rxjs';
 
 interface State {
     users: User[];
@@ -17,9 +18,19 @@ export class UsersService {
     #state = signal<State>({
         loading: true,
         users: [],
-    })
+    });
+
+    public users = computed( () => this.#state().users );
+    public loading = computed( () => this.#state().loading );
 
   constructor() {
-    this.http.get<UsersResponse>('https://reqres.in/api/users');
+    this.http.get<UsersResponse>('https://reqres.in/api/users')
+    .pipe( delay(1500))
+    .subscribe( res => {
+        this.#state.set({
+            loading: false,
+            users: res.data,
+    })
+    })
    }
 }

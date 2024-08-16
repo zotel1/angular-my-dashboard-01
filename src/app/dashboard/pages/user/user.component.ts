dfe142be76281457c444347749c17f12c294bcbc
zotel1 <zotelsigel@gmail.com>
2024-08-15 23:37:17 -0300

    update: user compinent

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -3,6 +3,9 @@ import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TitleComponent } from '@shared/title/title.component';
 import { User } from '../../../interfaces/req-response';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { UsersService } from '@services/users.service';
 
 @Component({
   standalone: true,
@@ -22,9 +25,15 @@ import { User } from '../../../interfaces/req-response';
 })
 export default class UserComponent {
     private route = inject( ActivatedRoute );
+    private usersService = inject( UsersService);
 
-    public user = signal<User | undefined>(undefined)
+   // public user = signal<User | undefined>(undefined)
 
+   public user = toSignal(
+    this.route.params.pipe(
+        switchMap( ({ id }) => this.usersService.getUserById( id ))
+    )
+   )
    // constructor() {
      //   console.log()
    // }
